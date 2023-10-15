02a7a7d708ed3a090e6f40ec3db958c121071f9c
AD776 <78531446+AD776@users.noreply.github.com>
2023-10-15 00:57:41 -0700

    Add files via upload
    UI Update

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function expandOptions() {
-	var x = document.getElementById("tops-menu");
+function expandOptions(id) {
+	var x = document.getElementById(id);
   if (x.className === "flex-hidden") {
     x.className = "flex-shown";
     console.log("show");
   } else {
     x.className = "flex-hidden";
+    console.log("hide");
+  }
+}
+
+function closeOptions(id){
+  var y = document.getElementById(id);
+  if (y.className === "flex-shown") {
+    y.className = "flex-hidden";
     console.log("hide");
   }
 }
